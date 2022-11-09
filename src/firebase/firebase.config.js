2e817469e7ddafce5e6427baeca8f053d4c068db
Ahmad Musa <amaahmadmusa@gmail.com>
2022-11-08 22:32:51 -0800

    firebase config added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPBth4H6Pw8QrJTQr3yUOrHhTtRDNSx4w",
+  authDomain: "visa-consultancy-service.firebaseapp.com",
+  projectId: "visa-consultancy-service",
+  storageBucket: "visa-consultancy-service.appspot.com",
+  messagingSenderId: "796018358943",
+  appId: "1:796018358943:web:07aa284669dabc499740aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
